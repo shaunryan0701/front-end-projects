253e9f4953cba61bb4738544f7aa93df85988df8
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-02-05 20:02:56 +0000

    Finished dad-jokes project

---
+++ dad-jokes/script.js
@@ -0,0 +1,29 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+//using async/await
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json'
+    }
+  }
+
+  const response = await fetch('https://icanhazdadjoke.com/', config)
+  const data = await response.json()
+
+  jokeEl.innerHTML = data.joke
+}
+
+// Using .then
+// function generateJoke() {
+//   fetch('https://icanhazdadjoke.com/', config)
+//   .then(response => response.json())
+//   .then(data => {
+//     jokeEl.innerHTML = data.joke
+//   })
+// }
